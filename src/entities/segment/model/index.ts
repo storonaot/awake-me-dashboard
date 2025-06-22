798c6a78cdf01df5bb92649d29c641b665b8f8c5
Eugene Zh <awake.me.stardust@gmail.com>
2025-06-22 15:26:46 +0300

    Remove Segment Logic

--- src/entities/segment/model/index.ts
+++
@@ -1,2 +0,0 @@
-export type { Segment } from './types'
-export { addSegmentAPI, getSegmentsAPI, updateSegmentAPI } from './segment.api'
--- src/features/segment/model/index.ts
+++
@@ -1,2 +0,0 @@
-export { useSegments } from './useSegments'
-export { useSegmentActions } from './useSegmentActions'
