aee651c668f750254a7fc338943456058d8e25de
Eugene Zh <awake.me.stardust@gmail.com>
2025-06-25 15:09:57 +0300

    Update vite conf

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
+const appName = process.env.VITE_APP_NAME || 'Awake Me'
+const port = parseInt(process.env.VITE_PORT || '5173')
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -10,5 +13,12 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  server: {
+    port,
+  },
+  clearScreen: false, // Чтобы логи не скрывались
+  define: {
+    __APP_NAME__: JSON.stringify(appName),
   },
 })
